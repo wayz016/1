c577bd56286e70dd4828627a7424f5d6f1d90053
wayz016 <92643057+wayz016@users.noreply.github.com>
2021-10-18 02:26:17 +0800

    Add files via upload

---
+++ 1/src/App.vue
@@ -0,0 +1,403 @@
+<template>
+  <div id="app">
+    <div id="content">
+      <StyleEditor ref="styleEditor" :code="currentStyle"></StyleEditor>
+      <ResumeEditor ref="resumeEditor" :markdown="currentMarkdown" :enableHtml="enableHtml"></ResumeEditor>
+    </div>
+    <div id="foot">
+      <ThankEditor ref="thankEditor" :markdown="currentThankMarkdown" :enableHtml="enableHtml"></ThankEditor>
+    </div>
+  </div>
+</template>
+
+<script>
+  import StyleEditor from './components/StyleEditor'
+  import ResumeEditor from './components/ResumeEditor'
+  import ThankEditor from './components/ThankEditor'
+  import './assets/reset.css'
+
+  export default {
+    name: 'app',
+    components: {
+      StyleEditor,
+      ResumeEditor,
+      ThankEditor
+    },
+    data() {
+      return {
+        interval: 5,
+        currentStyle: '',
+        enableHtml: false,
+        fullStyle: [
+          `/*
+* Inspired by http://strml.net/
+* 源码地址 https://github.com/sitexa/anires
+* 大家好，我是夹心饼干。
+* 我来写一份简历！
+*/
+
+/* 给所有元素加上过渡效果 */
+* {
+  transition: all .1s;
+}
+/* 设置背景颜色 */
+html {
+  color: rgb(222,222,222); background: rgb(0,64,64);
+}
+#content{
+  height:70vh;
+  margin:0;
+}
+#foot{
+  height:29vh;
+  margin:0;
+}
+
+/* 设置边框 */
+.styleEditor {
+  padding: .5em;
+  border: 1px solid;
+  margin: .5em;
+  overflow: auto;
+  width: 50vw; height: 70vh;
+  background: rgb(20,20,20);
+}
+/* 代码高亮 */
+.token.selector{ color: rgb(130,150,0); }
+.token.property{ color: rgb(190,140,0); }
+.token.punctuation{ color: yellow; }
+.token.function{ color: rgb(40,160,150); }
+
+/* 加3D效果 */
+html{
+  perspective: 1000px;
+}
+.styleEditor {
+  position: fixed; left: 0; top: 0;
+  -webkit-transition: none;
+  transition: none;
+  -webkit-transform: rotateY(10deg) translateZ(-100px) ;
+          transform: rotateY(10deg) translateZ(-100px) ;
+}
+
+/* 准备一个编辑器 */
+.resumeEditor{
+  position: fixed; right: 0; top: 0;
+  padding: .5em;  margin: .5em;
+  width: 50vw; height: 70vh;
+  border: 1px solid;
+  background: rgb(200,200,200); color: #222;
+  overflow: auto;
+  -webkit-transition: none;
+  transition: none;
+  -webkit-transform: rotateY(-10deg) translateZ(-100px) ;
+          transform: rotateY(-10deg) translateZ(-100px) ;
+}
+/* 开始写简历 */
+`, `
+/*将Markdown格式翻译成HTML
+ *再对HTML加点样式
+*/
+.resumeEditor{
+  padding: 2em;
+}
+.resumeEditor h2{
+  display: inline-block;
+  border-bottom: 1px solid;
+  margin: 1em 0 .5em;
+}
+.resumeEditor ul,.resumeEditor ol{
+  list-style: none;
+}
+.resumeEditor ul> li::before{
+  content: '•';
+  margin-right: .5em;
+}
+.resumeEditor ol {
+  counter-reset: section;
+}
+.resumeEditor ol li::before {
+  counter-increment: section;
+  content: counters(section, ".") " ";
+  margin-right: .5em;
+}
+.resumeEditor blockquote {
+  margin: 1em;
+  padding: .5em;
+  background: #ddd;
+}
+`, `/* 写封感谢信。
+ * 感谢大家对我的关注。
+ */
+.styleEditor{
+    width:50vw;height:70vh;
+}
+
+.resumeEditor{
+   width:50vw;height:70vh;
+}
+
+.thankEditor{
+  position: relative; left: 0; top: 0;
+  background: #E9D9BB;
+  color: #001C42;
+  overflow: auto;
+}
+
+.thankEditor {
+  width: 99vw; height: 45vh;
+  border: 1px solid #ccc;
+  font-size: .9em;
+}
+`,`
+.thankEditor{
+  padding: .5em;  margin: .5em; margin-top:1em;
+}
+
+.thankEditor ul,.thankEditor ol{
+  list-style: none;
+}
+.thankEditor ul> li::before{
+  content: '☞'; color: red;
+  margin-right: .5em;
+}
+.thankEditor ol {
+  counter-reset: section;
+}
+.thankEditor ol li::before {
+  counter-increment: section;
+  content: counters(section, "☞") " ";
+  margin-right: .5em;
+}
+
+.thankEditor{
+  width: 99vw; height: 45vh;
+  -webkit-transition: none;
+  transition: none;
+  -webkit-transform: rotateX(-10deg) translateZ(-200px);
+          transform: rotateX(-10deg) translateZ(-200px);
+}
+
+`],
+        currentMarkdown: '',
+        currentThankMarkdown: '',
+        fullMarkdown: `夹心饼干
+====
+
+坐标：江苏昆山。
+
+房地产经纪人，汽车中级工，米粉。现为自由职业。
+
+技能
+====
+
+我的世界
+----
+  - 未来城腐竹
+  - 服务器后台调试
+  - 第三方登录(qq)
+
+后端开发
+----
+  - 用户管理
+  - 单点登录
+  - 第三方登录
+  - 权限管理
+  - 交易系统
+  - 支付系统
+  - 社区系统
+  - 博客系统
+  - 公众号开发
+  - 小程序开发
+  - API接口开发
+
+米家
+----
+  - 米家智能化调试
+  - homekit调试
+
+专业证书
+----
+  - 计算机应用能力中级
+  - 汽车维修中级工
+  - 通用英语
+  - 一级ms office
+  - 普通话二级乙等
+  - 驾照c1
+  - 低压电工
+  - 房地产经纪人
+
+技术及语言
+----
+  - Java: SpringMVC, SpringCloud, Hibernate, iBatis, spark, sql2o, HikariCP, freemarker, okHttp, retrofit, RxJava
+  - Kotlin: ktor, exposed, anko
+  - Node.js: express, angular, ionic, react, cordova, meteor, electron, axios
+  - Swift: Vapor, ReactiveSwift
+  - Golang: hugo, beego, gorm, sqlx, matcha
+  - Python: tushare, pandas, numpy, matplotlib
+  - DotNet and PHP
+  - DB: SQLServer, Oracle, MySQL/MariaDB, MongoDB, graphQL, redis, memcached
+  - WebServer: apache, nginx, tomcat, netty, jetty
+  - OS: Ubuntu, CentOS, MacOS, Windows
+  - Others: Docker, git, Xmind，Axure
+
+工作经历
+----
+
+1. 昆山港龙建材有限公司
+2. 昆山神州通房地产开发有限公司
+3. 昆山某奥口腔门诊部
+4. 昆山现代电梯工程有限公司
+5. 昆山市中茵宏鑫商贸有限公司
+
+教育经历
+----
+
+1. 昆山登云学院 
+2. 南京理工大学
+
+自我评价
+----
+
+工作踏实、勤劳、具有极强的的责任感，上进心强。对每件事都不
+折不扣地完成,“要做就最好”这是我处理每件事的目标。自我控制
+力与自学能力强，有良好的沟通能力、协调能力及较强的服务意识。
+对网络等新兴事物有着浓厚的兴趣，例如web建站、服务器维
+护以及搭建，SQL数据库的简单维护、简易E语言编程、计算机的
+组装等等。
+能够注重团队协作，善于沟通协调;喜欢学习和接受新事物，深
+信有耕耘就会有收获。听从领导指挥愿意从基层做起 。
+
+
+
+链接
+----
+
+* [个人博客](http://wayz016.oicp.net)
+
+勾引方式
+----
+
+* 微信：wayz016
+
+`, thanksMarkdown: `
+鸣谢
+----
+
+* 这段时间，我有些诚煌诚恐。
+* 我对自己也有了一个新的认识。我逐渐发现，我不是去找一份工作，而是在寻找一些机会，一些志同道合的伙伴，一份激动人心的事业。
+* 很荣幸，已经有几个朋友跟我达成了意向，准备组建团队。
+* 到底做什么项目呢？
+
+
+  `
+      }
+    },
+    created() {
+      this.makeResume()
+    },
+
+    methods: {
+      makeResume: async function () {
+        await this.progressivelyShowStyle(0);
+        await this.progressivelyShowResume();
+        await this.progressivelyShowStyle(1);
+        await this.showHtml();
+        await this.progressivelyShowStyle(2);
+        await this.progressivelyShowThanks();
+        await this.progressivelyShowStyle(3)
+      },
+      showHtml() {
+        return new Promise((resolve, reject) => {
+          this.enableHtml = true
+          resolve()
+        })
+      },
+      progressivelyShowStyle(n) {
+        return new Promise((resolve, reject) => {
+          let interval = this.interval
+          let showStyle = (async function () {
+            let style = this.fullStyle[n]
+            if (!style) {
+              return
+            }
+            // 计算前 n 个 style 的字符总数
+            let length = this.fullStyle.filter((_, index) => index <= n).map((item) => item.length).reduce((p, c) => p + c, 0)
+            let prefixLength = length - style.length
+            if (this.currentStyle.length < length) {
+              let l = this.currentStyle.length - prefixLength
+              let char = style.substring(l, l + 1) || ' '
+              this.currentStyle += char
+              if (style.substring(l - 1, l) === '\n' && this.$refs.styleEditor) {
+                this.$nextTick(() => {
+                  this.$refs.styleEditor.goBottom()
+                })
+              }
+              setTimeout(showStyle, interval)
+            } else {
+              resolve()
+            }
+          }).bind(this)
+          showStyle()
+        })
+      },
+      progressivelyShowResume() {
+        return new Promise((resolve, reject) => {
+          let length = this.fullMarkdown.length
+          let interval = this.interval
+          let showResume = () => {
+            if (this.currentMarkdown.length < length) {
+              this.currentMarkdown = this.fullMarkdown.substring(0, this.currentMarkdown.length + 1)
+              let lastChar = this.currentMarkdown[this.currentMarkdown.length - 1]
+              let prevChar = this.currentMarkdown[this.currentMarkdown.length - 2]
+              if (prevChar === '\n' && this.$refs.resumeEditor) {
+                this.$nextTick(() => this.$refs.resumeEditor.goBottom())
+              }
+              setTimeout(showResume, interval)
+            } else {
+              resolve()
+            }
+          }
+          showResume()
+        })
+      },
+      progressivelyShowThanks() {
+        return new Promise((resolve, reject) => {
+          let length = this.thanksMarkdown.length
+          let interval = this.interval
+          let showThanks = () => {
+            if (this.currentThankMarkdown.length < length) {
+              this.currentThankMarkdown = this.thanksMarkdown.substring(0, this.currentThankMarkdown.length + 1)
+              let lastChar = this.currentThankMarkdown[this.currentThankMarkdown.length - 1]
+              let prevChar = this.currentThankMarkdown[this.currentThankMarkdown.length - 2]
+              if (prevChar === '\n' && this.$refs.thankEditor) {
+                this.$nextTick(() => this.$refs.thankEditor.goBottom())
+              }
+              setTimeout(showThanks, interval)
+            } else {
+              resolve()
+            }
+          }
+          showThanks()
+        })
+      }
+    }
+  }
+
+</script>
+
+<style scoped>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  html {
+    min-height: 100vh;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+</style>
